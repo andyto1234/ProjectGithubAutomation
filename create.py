85993ef66a20a3ab96f52cc86ab8cb2532dc996a
andyto1234 <toshuho@gmail.com>
2021-03-24 11:50:46 +0000

    Initial commit

---
+++ create.py
@@ -0,0 +1,33 @@
+import sys
+import os
+from github import Github
+from dotenv import load_dotenv
+
+load_dotenv()
+
+path = os.getenv("FILEPATH")
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+token = os.getenv("TOKEN")
+g = Github(token)
+
+def create():
+    folderName = str(sys.argv[1])
+    os.makedirs(path + str(folderName))
+    user = g.get_user()
+    repo = user.create_repo(folderName)
+    print("Succesfully created repository {}".format(folderName))
+    commands = [f'cd {path} + {str(folderName)}',
+        f'echo "# {repo.name}" >> README.md',
+        'git init',
+        f'git remote add origin https://github.com/{username}/{folderName}.git',
+        'git add .',
+        'git commit -m "Initial commit"',
+        'git push -u origin master',
+        'cp /Users/ato/scripts/website_code/andyto.uk/push .']
+    for c in commands:
+        os.system(c)
+    print("Succesfully pushed {} to GitHub".format(folderName))
+
+if __name__ == "__main__":
+    create()
