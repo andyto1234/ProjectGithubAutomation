71090b588d8bbda68609651eba40dbcfdf553e94
andyto1234 <toshuho@gmail.com>
2021-03-24 14:45:49 +0000

    update

--- create.py
+++ create.py
@@ -17,17 +17,17 @@ def create():
     user = g.get_user()
     repo = user.create_repo(folderName)
     print("Succesfully created repository {}".format(folderName))
-    commands = [f'cd {path} + {str(folderName)}',
-        f'echo "# {repo.name}" >> README.md',
-        'git init',
-        f'git remote add origin https://github.com/{username}/{folderName}.git',
-        'git add .',
-        'git commit -m "Initial commit"',
-        'git push -u origin master',
-        'cp /Users/ato/scripts/website_code/andyto.uk/push .']
-    for c in commands:
-        os.system(c)
-    print(f"Succesfully pushed {folderName} to GitHub")
+    # commands = [f'cd {path} + {str(folderName)}',
+    #     f'echo "# {repo.name}" >> README.md',
+    #     'git init',
+    #     f'git remote add origin https://github.com/{username}/{folderName}.git',
+    #     'git add .',
+    #     'git commit -m "Initial commit"',
+    #     'git push -u origin master',
+    #     'cp /Users/ato/scripts/website_code/andyto.uk/push .']
+    # for c in commands:
+    #     os.system(c)
+    # print(f"Succesfully pushed {folderName} to GitHub")
 
 if __name__ == "__main__":
     create()
